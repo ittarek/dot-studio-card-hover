82a9b323a703dcefce661598388e96eed32745fc
MD Tariqul Islam <ittarek551@gmail.com>
2024-03-07 15:29:33 +0600

    add my code

---
+++ index.js
@@ -0,0 +1,54 @@
+
+// fetch data
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+       handleDisplay(data)})
+    .catch(error => console.error("Error fetching JSON data:", error));
+});
+
+// display Data Function
+const handleDisplay = (data)=>{
+const cardId = document.getElementById("cards");
+data.forEach(el => {
+        console.log(el.Description);
+    const div = document.createElement("div");
+    const titleWords = el.Title.split(" ");
+    const firstWord = titleWords.length > 0 ? titleWords[0] : "";
+    const secondWord = titleWords.length > 1 ? titleWords[1] : "";
+    const thirdWord = titleWords.length > 2 ? titleWords[2] : "";
+    const fourthWord = titleWords.length > 3 ? titleWords[3] : "";
+    const fiveWord = titleWords.length > 4 ? titleWords[4] : "";
+    div.className = "card";
+      div.style.backgroundImage = `url('${el.image}')`;
+    div.innerHTML = `
+ 
+
+     <div class="card-header">
+    <h5 class="card-title">
+    <span>${firstWord}</span>
+    <span>${secondWord}</span>
+    <span>${thirdWord}</span>
+    <span>${fourthWord}</span>
+    <span>${fiveWord}</span>
+        </h5> </div>
+    
+   <div class="hoverItem">
+    <h2 class="hoverTitle"> <span>${firstWord}</span>
+    <span>${secondWord}</span>
+    <span>${thirdWord}</span>
+    <span>${fourthWord}</span>
+    <span>${fiveWord}</span></h2>
+    <p class="hoverDsc">${el.Description}</p>
+    </div>
+    
+
+    
+    `;
+    cardId.appendChild(div)
+    
+});
+}
+
+// handleDisplay()
